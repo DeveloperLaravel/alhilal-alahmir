00e3af877b503fb44ad615f210ea81b95d90c182
Developerlaravel <hnarfr20063@github.com>
2025-05-29 01:18:32 +0200

    project end

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,13 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(),
-    );
+    return MaterialApp(home: const MyHomePage());
   }
 }
 
@@ -34,12 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: const Text('You have pushed the button this many times:'),
-      ),
+      body: Center(),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
