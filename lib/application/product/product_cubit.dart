9040ebb758d0595efb5feb398c5b40772acbd81d
Developerlaravel <hnarfr20063@github.com>
2025-05-31 11:02:08 +0200

    finsf get data with cubic

--- lib/application/product/product_cubit.dart
+++ lib/application/product/product_cubit.dart
@@ -1,13 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:projectimagefirb/data/repositories_product.dart';
 import 'package:projectimagefirb/domain/models_product.dart';
-import 'package:meta/meta.dart';
 
 part 'product_state.dart';
 
 class ProductCubit extends Cubit<ProductState> {
-  ProductCubit() : super(ProductLoading());
+  ProductCubit() : super(const ProductLoading());
+  RepositoriesProduct repositoriesProduct = RepositoriesProduct();
 
-  getProductDate() {}
+  getProductDate() async {
+    try {
+      final List<ModelsProduct> products = await repositoriesProduct
+          .getProductDate();
+      emit(ProductLoaded(products));
+    } catch (e) {
+      emit(ProductError(e.toString()));
+    }
+  }
 }
